f19f41e19d2669216f1775573dc8d5b5bd3d6fe2
yamatamay0229 <yamatamay0229@gmail.com>
2025-08-28 08:34:47 +0900

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -212,6 +212,7 @@ function contrastRatio(hex1, hex2){
  * @returns 調整後のhex
  */
 function adjustContrastNear(hex, bg, min=3){
+  console.log('adjustContrastNear hit', hex, bg, min);
   if (!hex) return null;
   const origRgb = hexToRgb(hex), bgRgb = hexToRgb(bg);
   if (!origRgb || !bgRgb) return hex;
