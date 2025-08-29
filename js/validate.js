239d5f9a6de1c8224b38d949237602f2d7c5e6f2
yamatamay0229 <yamatamay0229@gmail.com>
2025-08-29 11:28:30 +0900

    Create validate.js

---
+++ js/validate.js
@@ -0,0 +1,5 @@
+export function validateCharacter(/*c*/){ return { ok:true, errors:[] }; }
+export function validateRelation(/*r*/){ return { ok:true, errors:[] }; }
+export function validateGroup   (/*g*/){ return { ok:true, errors:[] }; }
+export function assertUniqueId  (/*type,id*/){ /* TODO */ }
+export function exists(/*type,id*/){ return true; }
