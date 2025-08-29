ade06ca930286cd0edd3789cb5a909c06d59e1bb
yamatamay0229 <yamatamay0229@gmail.com>
2025-08-29 11:27:35 +0900

    Create io.js

---
+++ js/io.js
@@ -0,0 +1,4 @@
+export function exportJSON(){ /* TODO: state を文字列化してダウンロード */ }
+export function importJSON(/*text*/){ /* TODO: setData */ }
+export function exportCSV(/*type*/) { /* TODO */ }
+export function importCSV(/*type, text*/) { /* TODO */ }
