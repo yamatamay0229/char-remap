6ca5b094c6977ba7a1cc664b567483d7ee502e0f
yamatamay0229 <yamatamay0229@gmail.com>
2025-08-29 11:24:33 +0900

    Create nodeView.js

---
+++ js/views/nodeView.js
@@ -0,0 +1,11 @@
+// ノードのstyle片とイベント束ね（中身は後で）
+
+export function nodeStyle(){
+  return []; // cytoscape style セレクタ配列を返す想定
+}
+export function bindNodeEvents(/*cy*/){
+  // TODO: dblclick / hover / contextmenu
+}
+export function formatNodeLabel(/*data*/){
+  return ''; // TODO
+}
