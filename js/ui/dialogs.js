d0973b4112f64064497a74f50df2bd7eb821d07f
yamatamay0229 <yamatamay0229@gmail.com>
2025-08-29 11:26:18 +0900

    Create dialogs.js

---
+++ js/ui/dialogs.js
@@ -0,0 +1,6 @@
+// モーダル類（後で本実装）
+export async function openNewCharacterDialog(){ /* TODO return {id?, name} */ }
+export async function openNewRelationDialog(){ /* TODO return {from,to,label?,strength?} */ }
+export async function openSheetManagerDialog(){ /* TODO */ }
+export async function confirm(/*message*/){ return true; }
+export async function prompt(/*fields*/){ return null; }
